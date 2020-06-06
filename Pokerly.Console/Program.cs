7be0c0d0e2b27deca9d6bc6f9e2bd71aee9738b4
Vinoth Sabanadesan <vinoth15@gmail.com>
2020-06-06 01:27:25 -0400

    init

---
+++ Pokerly.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pokerly.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
